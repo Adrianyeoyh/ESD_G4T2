825c20865738c1735c7e6cb88e9eb6665c7df410
adrian <adrianallinone@gmail.com>
2026-03-22 14:00:18 +0800

    created pseudo routes for payment service

--- backend/services/payment_service/app/routes/boilerplate_routes.py
+++ backend/services/payment_service/app/routes/boilerplate_routes.py
@@ -13,16 +13,11 @@ from backend.services.boilerplate_service.app.controllers.boilerplate_controller
     delete_invoice,
 )
 
-invoice_bp = Blueprint("invoice_bp", __name__)
+payment_bp = Blueprint("payment_bp", __name__)
 
 
-invoice_bp.route("/invoice", methods=["POST"])(create_invoice)
-invoice_bp.route("/invoice", methods=["GET"])(list_invoices)
-invoice_bp.route("/invoice/<int:invoice_id>", methods=["GET"])(get_invoice)
-invoice_bp.route("/invoice/record/<int:record_id>", methods=["GET"])(get_invoice_by_record_id)
-invoice_bp.route("/invoice/<int:invoice_id>/payment-pending", methods=["PUT"])(mark_payment_pending)
-invoice_bp.route("/invoice/<int:invoice_id>/paid", methods=["PUT"])(mark_paid)
-invoice_bp.route("/invoice/<int:invoice_id>/failed", methods=["PUT"])(mark_failed)
-invoice_bp.route("/invoice/<int:invoice_id>/retry", methods=["PUT"])(increment_retry)
-invoice_bp.route("/invoice/<int:invoice_id>/total", methods=["PUT"])(update_total)
-invoice_bp.route("/invoice/<int:invoice_id>", methods=["DELETE"])(delete_invoice)
+payment_bp.route("payments/intent", methods=["POST"]) ()
+payment_bp.route("payments/<int:payment_id>", methods=["GET"]) ()
+payment_bp.route("/payments/intent/<payment_intent_id>", methods=["GET"]) ()
+payment_bp.route("/payments/webhook", methods=["POST"]) ()
+payment_bp.route("//payments/retry", methods=["POST"]) ()
