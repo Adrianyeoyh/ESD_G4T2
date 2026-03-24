f068d31b1ba4f7ea6c3e3c03b0461c06dc3c5eb1
adrian <adrianallinone@gmail.com>
2026-03-24 15:29:36 +0800

    Feature:initial notification service

--- backend/services/notification_service/run.py
+++ backend/services/notification_service/run.py
@@ -1,6 +1,5 @@
-from app import create_app
-
-app = create_app()
+from app.services.consumer_service import NotificationConsumer
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=True)
+    consumer = NotificationConsumer()
+    consumer.start()
