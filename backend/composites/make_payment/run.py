bb97f720d10bc252feb1db5dec61f9f793ed6899
adrian <adrianallinone@gmail.com>
2026-03-27 15:12:17 +0800

    Fix: debug off when in production

--- backend/composites/make_payment/run.py
+++ backend/composites/make_payment/run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from app.config.settings import APP_ENV
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5005, debug=True)
+    app.run(host="0.0.0.0", port=5005, debug=(APP_ENV != "docker"))
