0b2d2fec28370cdfd6ee0857bb59d6df5978a4db
adrian <adrianallinone@gmail.com>
2026-03-27 18:06:36 +0800

    fix: change debug

--- backend/services/drug_catalogue_service/run.py
+++ backend/services/drug_catalogue_service/run.py
@@ -1,3 +1,5 @@
 import uvicorn
+from app.config.settings import APP_ENV
+
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5001, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=5001, reload=(APP_ENV != "docker")))
