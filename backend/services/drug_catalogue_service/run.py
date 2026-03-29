228ac1f85038e74cd7cadc1bdcf5dcc08925f5e4
adrian <adrianallinone@gmail.com>
2026-03-29 17:19:16 +0800

    Fix: compile error

--- backend/services/drug_catalogue_service/run.py
+++ backend/services/drug_catalogue_service/run.py
@@ -2,4 +2,4 @@ import uvicorn
 from app.config.settings import APP_ENV
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5001, reload=(APP_ENV != "docker")))
+    uvicorn.run("app:app", host="0.0.0.0", port=5001, reload=(APP_ENV != "docker"))
